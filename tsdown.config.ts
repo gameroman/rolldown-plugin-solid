bc67a184f37c4383f62ddfe0f3f969acf34b81e3
Roman <dev@rman.dev>
2026-01-12 21:41:33 +0000

    drop cjs support

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'tsdown';
 
-function createConfig(format: 'cjs' | 'esm') {
-  return defineConfig({
-    entry: ['src/index.ts'],
-    outDir: 'dist',
-    platform: 'node',
-    format: [format],
-    clean: true,
-    dts: format === 'esm',
-    target: 'node18',
-    minify: false,
-  });
-}
-
-export default [createConfig('cjs'), createConfig('esm')];
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  platform: 'node',
+  format: 'esm',
+  clean: true,
+  dts: true,
+  target: 'node18',
+  minify: false,
+});
