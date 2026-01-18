aca1b1e081e326dde25de53d040f48dc0852d0b4
Roman <dev@rman.dev>
2026-01-18 20:50:12 +0000

    inline @babel/preset-typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { parse } from "node:path";
 import { type TransformOptions, transformAsync } from "@babel/core";
-// @ts-expect-error: Babel types are not installed
-import ts from "@babel/preset-typescript";
+import ts from "./preset-typescript";
 import jsxTransform from "./babel-plugin-jsx-dom-expressions";
 import type { RolldownPlugin } from "rolldown";
 
