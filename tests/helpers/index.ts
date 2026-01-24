fbaeca60e3685ac76e96465487eae00564149d9e
Roman <dev@rman.dev>
2026-01-24 00:09:03 +0000

    Update index.ts

--- tests/helpers/index.ts
+++ tests/helpers/index.ts
@@ -18,35 +18,40 @@ export function createFixtureTest(
   fixture: string,
   options?: FixtureOptions,
 ) {
-  describe("rolldown-plugin-solid", () => {
+  describe("rolldown-plugin-solid", async () => {
+    const srcResult = await build({
+      platform: options?.platform ?? "browser",
+      input: resolve(testDir, fixture),
+      plugins: [solidPluginSrc(options?.plugin)],
+      output: { format: "esm" },
+      write: false,
+    });
+    const srcCode = srcResult.output[0].code;
+
+    const distResult = await build({
+      platform: options?.platform ?? "browser",
+      input: resolve(testDir, fixture),
+      plugins: [solidPluginDist(options?.plugin)],
+      output: { format: "esm" },
+      write: false,
+    });
+
+    const distCode = distResult.output[0].code;
+
     describe("src", () => {
       it(`should transform ${name}`, async () => {
-        const result = await build({
-          platform: options?.platform ?? "browser",
-          input: resolve(testDir, fixture),
-          plugins: [solidPluginSrc(options?.plugin)],
-          output: { format: "esm" },
-          write: false,
-        });
-
-        const code = result.output[0].code;
-        expect(code).toMatchSnapshot();
+        expect(srcCode).toMatchSnapshot();
       });
     });
 
     describe("dist", () => {
       it(`should transform ${name}`, async () => {
-        const result = await build({
-          platform: options?.platform ?? "browser",
-          input: resolve(testDir, fixture),
-          plugins: [solidPluginDist(options?.plugin)],
-          output: { format: "esm" },
-          write: false,
-        });
-
-        const code = result.output[0].code;
-        expect(code).toMatchSnapshot();
+        expect(distCode).toMatchSnapshot();
       });
+    });
+
+    it(`have same results when transforming ${name} for src and dist `, async () => {
+      expect(srcCode).toBe(distCode);
     });
   });
 }
